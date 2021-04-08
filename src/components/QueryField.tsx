bcfc23536fe5bed5d271ac382dc54076c5dfed1f
knogu <kotaronoguchi61@gmail.com>
2021-04-08 19:29:29 +0900

    余計な記述を削除

--- src/components/QueryField.tsx
+++ src/components/QueryField.tsx
@@ -13,45 +13,43 @@ export const QueryField = (props: Props) => {
 
   return (
     <div className="search">
-      <div className='common-filter-params'>
-        <h3 className='query-header'>{wayName}</h3>
-          <div className='query-body'>
-          <TimeField
-            hour={ query.startAfter.paddedHour() }
-            minute={ query.startAfter.paddedMinute() }
-            leave={ true }
-            onHourChange={(hour) =>{
-              const newQuery = { ...query, startAfter: new Time(Number(hour), Number(query.startAfter.minute)) }
-              setQuery(newQuery);
-            }}
-            onMinuteChange={(minute) =>{
-              const newQuery = { ...query, startAfter: new Time(Number(query.startAfter.hour), Number(minute)) }
-              setQuery(newQuery);
-            }}
-          />
+      <h3 className='query-header'>{wayName}</h3>
+        <div className='query-body'>
+        <TimeField
+          hour={ query.startAfter.paddedHour() }
+          minute={ query.startAfter.paddedMinute() }
+          leave={ true }
+          onHourChange={(hour) =>{
+            const newQuery = { ...query, startAfter: new Time(Number(hour), Number(query.startAfter.minute)) }
+            setQuery(newQuery);
+          }}
+          onMinuteChange={(minute) =>{
+            const newQuery = { ...query, startAfter: new Time(Number(query.startAfter.hour), Number(minute)) }
+            setQuery(newQuery);
+          }}
+        />
 
-          <TimeField
-            hour={ query.reachBefore.paddedHour() }
-            minute={ query.reachBefore.paddedMinute() }
-            leave={ false }
-            onHourChange={(hour) =>{
-              const newQuery = { ...query, reachBefore: new Time(Number(hour), Number(query.reachBefore.minute)) }
-              setQuery(newQuery);
-            }}
-            onMinuteChange={(minute) =>{
-              const newQuery = { ...query, reachBefore: new Time(Number(query.reachBefore.hour), Number(minute)) }
-              setQuery(newQuery);
-            }}
+        <TimeField
+          hour={ query.reachBefore.paddedHour() }
+          minute={ query.reachBefore.paddedMinute() }
+          leave={ false }
+          onHourChange={(hour) =>{
+            const newQuery = { ...query, reachBefore: new Time(Number(hour), Number(query.reachBefore.minute)) }
+            setQuery(newQuery);
+          }}
+          onMinuteChange={(minute) =>{
+            const newQuery = { ...query, reachBefore: new Time(Number(query.reachBefore.hour), Number(minute)) }
+            setQuery(newQuery);
+          }}
+        />
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setQuery({ ...query, needsLaggageSpace: !query.needsLaggageSpace })}
+            checked={ query.needsLaggageSpace }
           />
-          <label>
-            <input
-              type='checkbox'
-              onChange={(e) => setQuery({ ...query, needsLaggageSpace: !query.needsLaggageSpace })}
-              checked={ query.needsLaggageSpace }
-            />
-            荷物置き場のある新幹線のみ
-          </label>
-        </div>
+          荷物置き場のある新幹線のみ
+        </label>
       </div>
     </div>
   );
